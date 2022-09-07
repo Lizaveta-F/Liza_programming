c9e2783ac4a9b9900e3b088c82c3a44847a19e2e
Lizaveta-F <102254914+Lizaveta-F@users.noreply.github.com>
2022-09-07 15:30:14 +0200

    Delete My_project directory

--- My_project/My_Project.py
+++
@@ -1,9 +0,0 @@
-import requests 
-from bs4 import BeautifulSoup
-
-jobs_in_wales = 'https://www.jobsinwales.com/results?t=Wales&r=10'
-r = requests.get(jobs_in_wales)
-soup = BeautifulSoup(r.text,'lxml')
-
-block = soup.find('li',class_='list-group-item job-list enhanced').find('div',class_='job-salary').text
-print(block[block.find('£')+1:block.find(' ')])
