bb74e1005beb9bb589e317acffa325f09f8a96e7
Lizaveta-F <102254914+Lizaveta-F@users.noreply.github.com>
2022-10-10 23:40:07 +0200

    Delete livingabroad_project/livingabroad directory

--- livingabroad_project/livingabroad/main/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-
-
-#показывает, какие таблички должны отображаться в таблице администратора
--- livingabroad_project/livingabroad/main/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-
-# Можно создавать классы, на основе которого будет создана табличка в базе данных
