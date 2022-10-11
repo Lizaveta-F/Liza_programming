0b7f17ee2f6d98455ebcc459e82bc36860709ad9
LIZAVETA FADEEVA <ellizabetta.fadeeva@gmail.com>
2022-10-11 22:36:33 +0200

    first commit

--- E-commerce Site/EcomWebSite/shop/admin.py
+++ E-commerce Site/EcomWebSite/shop/admin.py
@@ -3,6 +3,20 @@ from . models import Order, Product
 
 # Register your models here.
 
+admin.site.site_header = "E-commerce Site"
+admin.site.index_title = "Manage ABC Shopping"
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    def change_category_to_default(self,request,queryset):
+        queryset.update(caregory = "default")
+    
+    change_category_to_default.short_description = 'Defaul category'
+    list_display = ('title', 'price', 'discount_price', 'caregory', 'description',)
+    search_fields = ('title','caregory','description',)
+    actions = ('change_category_to_default',)
+    list_editable = ('price','caregory',)
+    
+    
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Order)
